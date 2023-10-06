fe93311dacb7b1faf79111fc0713e09aff87a1bd
MIngmar Gyalzen Sherpa <mingmarsherpa@MIngmars-MacBook-Air.local>
2023-10-05 20:21:05 -0700

    showing the course

---
+++ index.js
@@ -0,0 +1,10 @@
+const user = {
+    math: '15rd position',
+    science: '5rd position',
+    computer: '10th position',
+  };
+  
+  const myVal = Object.values(user);
+  console.log(myVal);
+  
+ 
